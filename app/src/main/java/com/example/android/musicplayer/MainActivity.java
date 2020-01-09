d0572bc99dd2a34c9fe3c844c573cf4b51fd52d1
Toetra <tsitohaina.toetra.rak@gmail.com>
2020-01-09 14:56:10 +0300

    Play and Pause added

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.android.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mPlay;
+    private Button mPause;
+    private MediaPlayer mediaplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaplayer = MediaPlayer.create(this,R.raw.song);
+
+        mPlay = findViewById(R.id.play);
+        mPlay.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this, "Playing Sound",
+                        Toast.LENGTH_SHORT).show();
+                mediaplayer.start();
+
+            }
+        });
+        mPause = findViewById(R.id.pause);
+        mPause.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this, "Pausing Sound",
+                        Toast.LENGTH_SHORT).show();
+                mediaplayer.pause();
+
+            }
+        });
     }
+
+
+
 }
